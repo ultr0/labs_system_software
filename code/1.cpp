decb39325060ee5c0aea1f00b3b790e7a0d784f4
Roman Okhotnikov <mail@okht.ru>
2017-12-08 13:25:52 +1000

    делаем вторую лабу

--- code/1.cpp
+++ code/1.cpp
@@ -2,23 +2,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include <sys/wait.h>
+#include <sys/wait.h> //библиотека ожидания системных процессов
 #include <signal.h>
 #include <time.h> //библиотека для расчёта времени
 
-int main(int argc, char *argv[])
+int main1(int argc, char *argv[])
 {
     int fork_pid = 0;
     int child_pids[7];
     for (int i=0; i<7; i++){
-        fork_pid = fork();
+        fork_pid = fork(); //создание потомка
+        sleep(rand()%7 + 3); //задержка от 3 до 10 секунд
         if (fork_pid == 0){
-            int timeout = (rand() + getpid()) % 10;
-            sleep(timeout);
-            printf("PID потомка: %d %d\n", getpid(), rand()%10);
-            exit(0);
+            printf("PID потомка: %d\n", getpid());
+            exit(0); //оповещение
         }
         child_pids[i] = fork_pid;
+        int u = kill(child_pids[i], SIGKILL);
+        printf("PID завершённого потомка: %d, %d\n", child_pids[i], u);
     }
     time_t timer;
     time(&timer);
@@ -28,6 +29,7 @@ int main(int argc, char *argv[])
         if (seconds > 30){
             for (int i=0; i<7; i++){
                 kill(child_pids[i], SIGKILL);
+                printf("PID завершённого потомка: %d\n", child_pids[i]);
             }
         }
     }
